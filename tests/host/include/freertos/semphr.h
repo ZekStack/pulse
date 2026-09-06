eaa52ad686a310db37613c0d9ccd404169ced853
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-06 20:48:47 +0200

    Implement normal mutex aliases in host fake

--- tests/host/include/freertos/semphr.h
+++ tests/host/include/freertos/semphr.h
@@ -9,14 +9,23 @@ extern "C" {
 struct FakeSemaphore;
 typedef struct FakeSemaphore *SemaphoreHandle_t;
 
-SemaphoreHandle_t xSemaphoreCreateMutexStatic(StaticSemaphore_t *buffer);
 SemaphoreHandle_t xSemaphoreCreateRecursiveMutex(void);
 SemaphoreHandle_t xSemaphoreCreateRecursiveMutexStatic(StaticSemaphore_t *buffer);
-BaseType_t xSemaphoreTake(SemaphoreHandle_t semaphore, TickType_t ticksToWait);
-BaseType_t xSemaphoreGive(SemaphoreHandle_t semaphore);
 BaseType_t xSemaphoreTakeRecursive(SemaphoreHandle_t semaphore, TickType_t ticksToWait);
 BaseType_t xSemaphoreGiveRecursive(SemaphoreHandle_t semaphore);
 void vSemaphoreDelete(SemaphoreHandle_t semaphore);
+
+static inline SemaphoreHandle_t xSemaphoreCreateMutexStatic(StaticSemaphore_t *buffer) {
+	return xSemaphoreCreateRecursiveMutexStatic(buffer);
+}
+
+static inline BaseType_t xSemaphoreTake(SemaphoreHandle_t semaphore, TickType_t ticksToWait) {
+	return xSemaphoreTakeRecursive(semaphore, ticksToWait);
+}
+
+static inline BaseType_t xSemaphoreGive(SemaphoreHandle_t semaphore) {
+	return xSemaphoreGiveRecursive(semaphore);
+}
 
 #ifdef __cplusplus
 }
