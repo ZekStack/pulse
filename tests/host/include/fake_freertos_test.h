b72df8650d2cbf16fa31f58ed205912d7864123a
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-06 20:40:32 +0200

    Keep stale waiter test helpers compatible

--- tests/host/include/fake_freertos_test.h
+++ tests/host/include/fake_freertos_test.h
@@ -14,3 +14,15 @@ void fakeResumeTaskNotifications();
 void fakeHoldCurrentEndWaiter();
 bool fakeWaitForHeldEndWaiter(uint32_t timeoutMs);
 void fakeReleaseHeldEndWaiters();
+
+inline void fakeHoldCurrentEventWaiter() {
+	fakeHoldCurrentEndWaiter();
+}
+
+inline bool fakeWaitForHeldEventWaiter(uint32_t timeoutMs) {
+	return fakeWaitForHeldEndWaiter(timeoutMs);
+}
+
+inline void fakeReleaseHeldEventWaiters() {
+	fakeReleaseHeldEndWaiters();
+}
