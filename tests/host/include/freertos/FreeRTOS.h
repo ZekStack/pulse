fc8f2a320cf4211f91507e37aa4623caa47c637b
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-06 20:37:14 +0200

    Model Strata static FreeRTOS contracts in host tests

--- tests/host/include/freertos/FreeRTOS.h
+++ tests/host/include/freertos/FreeRTOS.h
@@ -12,14 +12,35 @@ typedef uint32_t TickType_t;
 typedef uint32_t StackType_t;
 typedef uint32_t configSTACK_DEPTH_TYPE;
 
+typedef struct StaticTask_t {
+	uintptr_t opaque[8];
+} StaticTask_t;
+
+typedef struct StaticQueue_t {
+	uintptr_t opaque[8];
+} StaticQueue_t;
+
+typedef struct StaticSemaphore_t {
+	uintptr_t opaque[8];
+} StaticSemaphore_t;
+
 #define pdTRUE 1
 #define pdFALSE 0
 #define pdPASS 1
 #define pdFAIL 0
 #define portMAX_DELAY UINT32_MAX
 #define tskNO_AFFINITY ((BaseType_t)-1)
+#define tskIDLE_PRIORITY ((UBaseType_t)0)
 #define pdMS_TO_TICKS(ms) ((TickType_t)(ms))
+
+#define configSUPPORT_STATIC_ALLOCATION 1
+#define configUSE_MUTEXES 1
+#define configUSE_RECURSIVE_MUTEXES 1
+#define configUSE_TIMERS 1
+#define INCLUDE_vTaskDelete 1
+#define INCLUDE_vTaskDelay 1
 #define INCLUDE_uxTaskGetStackHighWaterMark 1
+#define INCLUDE_xTimerPendFunctionCall 1
 
 #ifdef __cplusplus
 }
