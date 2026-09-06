3bd40ee3411c9ffb0656350d05ed7a57d5e4c3f6
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-06 20:48:25 +0200

    Complete Strata mutex host declarations

--- tests/host/include/freertos/semphr.h
+++ tests/host/include/freertos/semphr.h
@@ -9,8 +9,11 @@ extern "C" {
 struct FakeSemaphore;
 typedef struct FakeSemaphore *SemaphoreHandle_t;
 
+SemaphoreHandle_t xSemaphoreCreateMutexStatic(StaticSemaphore_t *buffer);
 SemaphoreHandle_t xSemaphoreCreateRecursiveMutex(void);
 SemaphoreHandle_t xSemaphoreCreateRecursiveMutexStatic(StaticSemaphore_t *buffer);
+BaseType_t xSemaphoreTake(SemaphoreHandle_t semaphore, TickType_t ticksToWait);
+BaseType_t xSemaphoreGive(SemaphoreHandle_t semaphore);
 BaseType_t xSemaphoreTakeRecursive(SemaphoreHandle_t semaphore, TickType_t ticksToWait);
 BaseType_t xSemaphoreGiveRecursive(SemaphoreHandle_t semaphore);
 void vSemaphoreDelete(SemaphoreHandle_t semaphore);
